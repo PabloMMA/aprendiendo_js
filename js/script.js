5293f16f138af0e43b3469ca3957f33a411d602c
PabloMMA <101149439+PabloMMA@users.noreply.github.com>
2022-04-29 20:00:43 -0300

    primeros pasos

---
+++ js/script.js
@@ -0,0 +1,37 @@
+// Comentario de una sola linea
+/*comentario multilinea para usarn en
+varias linear, como porejmplo un bloque de código */
+
+//variable que puede cambiar en el futuro
+// let nombre = "Pablo";
+
+// //error de usar const con un dato que cambia
+// const apellido = "Meza";
+
+// // esta variable "const" es la que se utiliza para valores que no modifican
+// const IVA = 0.22;
+
+
+// //Las variables "Let" se pueden declarar de dos formas
+// //forma 1, es clarar la variable y darle una valor
+// let edad = 38;
+// //para cambiar el valor de una variable solo necesito nombrarla
+// edad = 40;
+
+// //forma 2, es crear la variable sin el valor
+// let lugarDeNacimiento;
+// //darle valor a una variable al mencionarla sin darselo al momento de crearla
+// lugarDeNacimiento = "Paysandú"
+
+// //las variables "const" se declaran solo de una manera, y se le da varlo al momento de declarla
+// const PI = 3.14;
+
+let numeroA = 38;
+let numeroB = 1;
+
+let edadProximoAnio = numeroA + numeroB;
+
+let name1= "Pablo";
+let lastName=" Meza";
+
+let fullName = name1+lastName;
